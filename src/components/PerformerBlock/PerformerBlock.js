2cb8255ca7595ce2fbac3a4a6fdba52f8e9e959d
Oleksandr Shpak <oleksandr.o.shpak@gmail.com>
2023-06-23 16:10:30 +0300

    getAllPerformers

--- src/components/PerformerBlock/PerformerBlock.js
+++ src/components/PerformerBlock/PerformerBlock.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import css from "../PerformerBlock/PerformerBlock.module.css";
-// public string Title { get; set; } = null!;
-//
-// public string? Description { get; set; }
-//
-// public int? CareerBeginYear { get; set; }
-//
-// public string? Img { get; set; }
-//
-// public virtual string? Country { get; set; }
-// public virtual List<string> PerformerGenres { get; set; } = new List<string>();
-// public virtual string? PerformerType { get; set; }
+
 const PerformerBlock = ({performer}) => {
     return (
         <div className={`${css.container}`}>
-            <img src={performer.image} alt="performer"/>
-            {/*<h3>{performer.title}</h3>*/}
-
+            <img src={performer.img} alt={`${performer.title}`}/>
             <div className={`${css.imageText}`}>{performer.title}</div>
         </div>
     );
---
+++ src/data/Connections.js
@@ -0,0 +1,5 @@
+const connections = {
+    get_all_performers: 'https://localhost:7020/api/Performer/get-all'
+}
+
+export {connections}
