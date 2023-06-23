eda5aceb1aae999cd08c641ca54aff5665a7a69c
Oleksandr Shpak <oleksandr.o.shpak@gmail.com>
2023-06-23 16:36:35 +0300

    before merge

--- src/App.js
+++ src/App.js
@@ -7,23 +7,10 @@ import {Layout} from "./components/Layout/Layout";
 
 import React from "react";
 import {NotFoundPage} from "./pages/NotFoundPage/NotFoundPage";
-import PerformerPage from "./pages/PerformerPage/PerformerPage";
+import PerformersPage from "./pages/PerformesPage/PerformersPage";
 
 
 export default function App() {
-    const performer = {
-        performerId: 1,
-        title: "Antytila +Taras",
-        description: "Antytila is a Ukrainian rock band that was formed in 2005 in the city of Lviv. The band\"s name translates to \"Antibodies\" in English, and they are known for their energetic performances and catchy melodies. The band consists of five members, including lead singer Taras Topolia, guitarist Yevhen Rohachevsky, bassist Bohdan Nyzhankivsky, drummer Denys Myzyuk, and keyboardist Andriy Halay. Antytila gained national popularity in Ukraine after their victory in the television show \"Chance\" in 2007. Since then, they have released multiple albums and singles and have performed at numerous concerts and festivals in Ukraine and abroad.",
-        careerBeginYear: null,
-        img: "https://novy.tv/wp-content/uploads/sites/96/2019/12/2019-11-17_00-45-33_Maximov-1.jpg",
-        country: "Ukraine",
-        performerGenres: [
-            "Pop-Rock",
-            "Pop"
-        ],
-        performerType: "band"
-    }
     return (
       <Routes>
 
@@ -31,7 +18,7 @@ export default function App() {
 
           {/*<Route index element={<ImagePage/>}/>*/}
           {/*<Route path={'admin'} element={<AdminPage/>}/>*/}
-          <Route path={'performer'} element={<PerformerPage performer={performer}/>}/>
+          <Route path={'performer'} element={<PerformersPage/>}/>
 
           <Route path={'*'} element={<NotFoundPage/>}/>
 
--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -1,42 +1,16 @@
 import React, {useState} from 'react';
-import css from './NotFoundPage.module.css'
 import {NavLink} from "react-router-dom";
-import PerformerBlock from "../../components/PerformerBlock/PerformerBlock";
-import {connections} from "../../data";
 
+import css from './NotFoundPage.module.css'
 
 const NotFoundPage = () => {
-    const [performers, setPerformers] = useState([])
-
-    const get = () => {
-        // request data
-        fetch(connections.get_all_performers)
-            .then(response => response.json())
-            .then((json) => {
-                console.log(json)
-                setPerformers(json)
-            })
-
-        // set data
-    }
-
     return (
         <div>
-            <button onClick={get}>get performers</button>
             <h2>Ooops... Something wrong happened.</h2>
             <p>The page you are looking for doesn't exist</p>
             <NavLink to={'/'}>
                 <div className={`}`}>Головна сторінка</div>
             </NavLink>
-
-            <h1>PerformerBlock</h1>
-            <div className={`${css.PerformerBlock}`}>
-            {
-                performers.map((elem)=>(
-                <PerformerBlock performer={elem}/>
-                ))
-            }
-            </div>
         </div>
     );
 };
