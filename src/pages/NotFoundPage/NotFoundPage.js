9b841e4d7869d54b5970bd78a8ce23529b635a61
Sofiia Behei <behey.sofiaa@gmail.com>
2023-06-25 02:50:23 +0300

    add map

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -1,18 +1,55 @@
-import React, {useState} from 'react';
-import {NavLink} from "react-router-dom";
+// import React, {useState} from 'react';
+// import {NavLink} from "react-router-dom";
+//
+// import css from './NotFoundPage.module.css'
+//
+// const NotFoundPage = () => {
+//     return (
+//         <div>
+//             <h2>Ooops... Something wrong happened.</h2>
+//             <p>The page you are looking for doesn't exist</p>
+//             <NavLink to={'/'}>
+//                 <div className={`}`}>Головна сторінка</div>
+//             </NavLink>
+//         </div>
+//     );
+// };
+//
+// export {NotFoundPage};
 
-import css from './NotFoundPage.module.css'
+import React from 'react';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import osm from "./osm-providers";
+import L from "leaflet";
 
+import css from "../NotFoundPage/NotFoundPage.module.css";
+import 'leaflet/dist/leaflet.css'
+
+const markerIcon = new L.Icon({
+    iconUrl: require("D:\\3.2\\практика\\TicketProjectFront\\src\\pages\\images\\marker.png"),
+
+    iconSize: [35,30],
+    iconAnchor:[17,46],
+    popupAnchor:[0, -46]
+});
 const NotFoundPage = () => {
+
+    const center = [51.505, -0.09];
+    const ZOOM_LEVEL = 13;
     return (
-        <div>
-            <h2>Ooops... Something wrong happened.</h2>
-            <p>The page you are looking for doesn't exist</p>
-            <NavLink to={'/'}>
-                <div className={`}`}>Головна сторінка</div>
-            </NavLink>
-        </div>
+            <MapContainer
+                          center={center}
+                          zoom={ZOOM_LEVEL}
+                          style={{ width: '900px', height: '500px' }} >>
+                <TileLayer url={osm.maptiler.url} attribution={osm.maptiler.attribution}/>
+                <Marker position={[51.505, -0.09]} icon={markerIcon}>
+
+                    <Popup>
+
+                    </Popup>
+                </Marker>
+
+            </MapContainer>
     );
 };
-
 export {NotFoundPage};
---
+++ src/pages/NotFoundPage/osm-providers.js
@@ -0,0 +1,7 @@
+export default{
+    maptiler:{
+        url:"https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=hMO1gAn6QqHkKK2LLUWU",
+         attribution:'<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+        // attribution:"Attribution goes here"
+    }
+}
